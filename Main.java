6813ca4fd0b9d77a6af3786335f0899a734578ed
Ebukaokafor123 <159133042+Ebukaokafor123@users.noreply.github.com>
2024-05-27 21:48:15 +0100

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+Public class Main {
+    public static void main(string[]args){
+        CMP202 printUser = new CMP202();
+        printUser.printuserdetails();
+        Person student = new 
+        Person ("Okafor", "ebuka","12345", "BHU/23/04/09/0072","123");
+        Person staff = new Person ("Staff Name", "Bingham", "234234", null,445);
+        System.out.printIn(staff);
+
+        //
+        System.out.printIn(person.getName())
+        //
+        system.out.printIn(person.setPassword("146132"));;
+        //person.password = "12345";
+    }
+}
